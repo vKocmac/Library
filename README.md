7274ac9e3f36ccc134d783d7a77ecd2e06a9a445
vKocmac <vkocmac@gmail.com>
2022-11-22 22:07:45 +0200

    cards

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
 # Library
 
 todo
-grid template
-turn of form if somewhere else clicked
-{
-  body.addEventListener("click",()=>{
-    console.log("iam in")
-    addBookForm.className = "add-book";
-    header.style.filter = "blur(0)";
-    footer.style.filter = "blur(0)";
-    openFormButton.style.filter = "blur(0)";
-    gridContainer.style.filter = "blur(0)";
-    addBookForm.reset(); //empty the form
-  })
-}
-if form is null display an alert
+footer always at the bottom and after many cards
+delete card
+sign in
+site explanation box 
--- app.js
+++ app.js
@@ -3,36 +3,41 @@ const body = document.querySelector("body");
 const main = document.querySelector("main");
 let gridContainer = document.createElement("div");
 let card = document.createElement("div");
+let formDiv = document.querySelector(".form-div");
+
+
+
 
 function Book(name, author, pages) {
   this.name = name;
   this.author = author;
   this.pages = pages;
 }
+
 const openFormButton = document.querySelector(".open-form-button")
 const addBookForm = document.querySelector(".add-book");
 const header = document.querySelector("header");
 const footer = document.querySelector("footer");
+
 openFormButton.addEventListener("click", (e) => {
   header.style.filter = "blur(2px)";
   footer.style.filter = "blur(2px)";
   openFormButton.style.filter = "blur(2px)";
   gridContainer.style.filter = "blur(2px)";
   addBookForm.className = "new-add-book";
-  e.stopPropagation(e)  // important this prevents event goes inside the form as child createElement
-  
-if(addBookForm){
-  body.addEventListener("click",()=>{
-    console.log("iam in");
-    addBookForm.className = "add-book";
-    header.style.filter = "blur(0)";
-    footer.style.filter = "blur(0)";
-    openFormButton.style.filter = "blur(0)";
-    gridContainer.style.filter = "blur(0)";
-    addBookForm.reset(); //empty the form
+  e.stopPropagation(); // important this prevents event goes inside the form as child Element
 
-  })
-}
+  if (addBookForm) {
+    body.addEventListener("click", () => {
+      console.log("iam in");
+      addBookForm.className = "add-book";
+      header.style.filter = "blur(0)";
+      footer.style.filter = "blur(0)";
+      openFormButton.style.filter = "blur(0)";
+      gridContainer.style.filter = "blur(0)";
+      addBookForm.reset(); //empty the form
+    })
+  }
 
   addButton.addEventListener("click", () => {
     addBookForm.className = "add-book";
@@ -41,11 +46,8 @@ if(addBookForm){
     openFormButton.style.filter = "blur(0)";
     gridContainer.style.filter = "blur(0)";
     addBookForm.reset(); //empty the form
-
   })
-
 });
-
 
 const addButton = document.querySelector(".add-button");
 const name = document.querySelector("#title");
@@ -53,7 +55,7 @@ const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
 if (addButton) {
   addButton.addEventListener("click", (e) => { //prevent from empty submit
-    if (name.value === '' || name.value === null) {
+    if (name.value === '' || name.value === null || author.value === '' || author.value === null || pages.value === '' || pages.value === null ) {
       console.log('where is the name');
       e.preventDefault();
     } else {
@@ -62,7 +64,6 @@ if (addButton) {
   })
 }
 
-
 function addBookToLibrary() {
 
   const newBook = new Book(name.value, author.value, Number(pages.value));
@@ -70,7 +71,6 @@ function addBookToLibrary() {
   console.log(myLibrary)
 
   createBookCard();
-
 }
 
 function createBookCard() {
@@ -78,19 +78,17 @@ function createBookCard() {
   myLibrary.forEach((item, i) => {
 
     let list = document.createElement("ul");
-
     let listName = document.createElement("li");
-
     let listAuthor = document.createElement("li");
-
     let listPages = document.createElement("li");
+
 
     list.className = "card";
     gridContainer.className = "grid-container";
     console.log(item.name)
-    listName.innerHTML = item.name;
-    listAuthor.innerHTML = item.author;
-    listPages.innerHTML = item.pages;
+    listName.innerHTML = `Book title: ${item.name}`;
+    listAuthor.innerHTML = `Author: ${item.author}`;
+    listPages.innerHTML = `${item.pages} pages`;
 
     list.appendChild(listName);
     list.appendChild(listAuthor);
@@ -100,3 +98,7 @@ function createBookCard() {
     main.appendChild(gridContainer);
   });
 }
+
+formDiv.addEventListener("click", (e) => {
+  e.stopPropagation(e); // important this prevents event goes inside the form as child Element
+})
