60cf65378d7fa6455efba51c18d871450c50c900
vKocmac <vkocmac@gmail.com>
2022-12-26 19:10:13 +0200

    replace factory fanction with class and insert functionality as method

--- app.js
+++ app.js
@@ -6,12 +6,110 @@ let card = document.createElement("div");
 let formDiv = document.querySelector(".form-div");
 let info = document.querySelector(".info");
 
-function Book(name, author, pages, read = "not yet") {
-  this.name = name;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+
+class Book {
+  constructor(name, author, pages, read){
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  createBookCard(){
+    card.innerHTML = ""; // for multiple card for each object in an array i have to empty first the card
+    myLibrary.forEach((item, i) => {
+
+      let list = document.createElement("ul");
+      let listName = document.createElement("li");
+      let listAuthor = document.createElement("li");
+      let listPages = document.createElement("li");
+      let deleteImg = document.createElement("img");
+
+      deleteImg.src = "images/delete-24.ico"
+      deleteImg.className = "img";
+
+      const toggleDiv = document.createElement("div");
+      toggleDiv.className = "toggle-div"
+      const toggleLabel = document.createElement("label");
+      toggleLabel.className = "switch";
+      const toggleInput = document.createElement("input");
+      toggleInput.type = "checkbox";
+      console.log(item.read)
+
+      toggleInput.checked = false;
+      const toggleSpan = document.createElement("span");
+      toggleSpan.className = "slider round";
+      const togglePara = document.createElement("p");
+      togglePara.innerHTML = "Mark as read"
+      if (item.read === "not yet") {
+        toggleInput.checked = false;
+      }
+      if (item.read === "yes") {
+        toggleInput.checked = true;
+        list.style = "background:white; border:solid 2px #fff; outline: solid 2px #3F4E4F";
+        listName.style = "font-weight: bold; color:#2C3333";
+        listAuthor.style = "font-weight: bold; color:#2C3333";
+        listPages.style = "font-weight: bold; color:#2C3333";
+        togglePara.style = "font-weight: bold; color:#2C3333";
+      }
+
+      list.className = "card";
+      gridContainer.className = "grid-container";
+      listName.innerHTML = `Book title: "${item.name}"`;
+      listAuthor.innerHTML = `Author: ${item.author}`;
+      listPages.innerHTML = `${item.pages} pages`;
+
+      listPages.className = "listPages";
+
+      toggleInput.addEventListener("click", (e) => {
+        console.log("clicked")
+        if (toggleInput.checked === true) {
+          item.read = "yes";
+          toggleInput.checked = true;
+          console.log(toggleInput.checked)
+          console.log(item);
+          list.style = "background:white; border:solid 2px #fff; outline: solid 2px #3F4E4F";
+          listName.style = "font-weight: bold; color:#2C3333";
+          listAuthor.style = "font-weight: bold; color:#2C3333";
+          listPages.style = "font-weight: bold; color:#2C3333";
+          togglePara.style = "font-weight: bold; color:#2C3333";
+
+        } else {
+          item.read = "not yet";
+          toggleInput.checked = false;
+          console.log(item);
+          list.style = "background:#3F4E4F";
+          list.style = "color: white";
+          listName.style = "color: white";
+          listAuthor.style = "color: white";
+          listPages.style = "color: white";
+          togglePara.style = "color: white";
+        }
+      })
+
+      list.appendChild(listName);
+      list.appendChild(listAuthor);
+      list.appendChild(listPages);
+      toggleDiv.appendChild(togglePara);
+      toggleDiv.appendChild(toggleLabel);
+      toggleLabel.appendChild(toggleInput);
+      toggleLabel.appendChild(toggleSpan);
+      toggleDiv.appendChild(deleteImg);
+      card.appendChild(list);
+      list.appendChild(toggleDiv);
+      gridContainer.appendChild(card);
+      main.appendChild(gridContainer);
+
+      if (toggleDiv.contains(deleteImg)) {
+        deleteImg.addEventListener("click", (e) => {
+          card.removeChild(list);
+          delete myLibrary[i];
+          e.stopPropagation();
+        })
+      }
+    });
+  }
+  }
+
 
 const openFormButton = document.querySelector(".open-form-button")
 const addBookForm = document.querySelector(".add-book");
@@ -66,102 +164,7 @@ function addBookToLibrary() {
   const newBook = new Book(name.value, author.value, Number(pages.value));
   myLibrary.push(newBook);
   console.log(myLibrary)
-  createBookCard();
-}
-
-function createBookCard() {
-  card.innerHTML = ""; // for multiple card for each object in an array i have to empty first the card
-  myLibrary.forEach((item, i) => {
-
-    let list = document.createElement("ul");
-    let listName = document.createElement("li");
-    let listAuthor = document.createElement("li");
-    let listPages = document.createElement("li");
-    let deleteImg = document.createElement("img");
-
-    deleteImg.src = "images/delete-24.ico"
-    deleteImg.className = "img";
-
-    const toggleDiv = document.createElement("div");
-    toggleDiv.className = "toggle-div"
-    const toggleLabel = document.createElement("label");
-    toggleLabel.className = "switch";
-    const toggleInput = document.createElement("input");
-    toggleInput.type = "checkbox";
-    console.log(item.read)
-
-    toggleInput.checked = false;
-    const toggleSpan = document.createElement("span");
-    toggleSpan.className = "slider round";
-    const togglePara = document.createElement("p");
-    togglePara.innerHTML = "Mark as read"
-    if (item.read === "not yet") {
-      toggleInput.checked = false;
-    }
-    if (item.read === "yes") {
-      toggleInput.checked = true;
-      list.style = "background:white; border:solid 2px #fff; outline: solid 2px #3F4E4F";
-      listName.style = "font-weight: bold; color:#2C3333";
-      listAuthor.style = "font-weight: bold; color:#2C3333";
-      listPages.style = "font-weight: bold; color:#2C3333";
-      togglePara.style = "font-weight: bold; color:#2C3333";
-    }
-
-    list.className = "card";
-    gridContainer.className = "grid-container";
-    listName.innerHTML = `Book title: "${item.name}"`;
-    listAuthor.innerHTML = `Author: ${item.author}`;
-    listPages.innerHTML = `${item.pages} pages`;
-
-    listPages.className = "listPages";
-
-    toggleInput.addEventListener("click", (e) => {
-      console.log("clicked")
-      if (toggleInput.checked === true) {
-        item.read = "yes";
-        toggleInput.checked = true;
-        console.log(toggleInput.checked)
-        console.log(item);
-        list.style = "background:white; border:solid 2px #fff; outline: solid 2px #3F4E4F";
-        listName.style = "font-weight: bold; color:#2C3333";
-        listAuthor.style = "font-weight: bold; color:#2C3333";
-        listPages.style = "font-weight: bold; color:#2C3333";
-        togglePara.style = "font-weight: bold; color:#2C3333";
-
-      } else {
-        item.read = "not yet";
-        toggleInput.checked = false;
-        console.log(item);
-        list.style = "background:#3F4E4F";
-        list.style = "color: white";
-        listName.style = "color: white";
-        listAuthor.style = "color: white";
-        listPages.style = "color: white";
-        togglePara.style = "color: white";
-      }
-    })
-
-    list.appendChild(listName);
-    list.appendChild(listAuthor);
-    list.appendChild(listPages);
-    toggleDiv.appendChild(togglePara);
-    toggleDiv.appendChild(toggleLabel);
-    toggleLabel.appendChild(toggleInput);
-    toggleLabel.appendChild(toggleSpan);
-    toggleDiv.appendChild(deleteImg);
-    card.appendChild(list);
-    list.appendChild(toggleDiv);
-    gridContainer.appendChild(card);
-    main.appendChild(gridContainer);
-
-    if (toggleDiv.contains(deleteImg)) {
-      deleteImg.addEventListener("click", (e) => {
-        card.removeChild(list);
-        delete myLibrary[i];
-        e.stopPropagation();
-      })
-    }
-  });
+  newBook.createBookCard();
 }
 
 formDiv.addEventListener("click", (e) => {
